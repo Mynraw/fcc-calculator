eefd78b9a41d1b58e32437680e1a21ad94949c4a
Arya Acikgoz <arya.acikgoz@ankaref.com>
2023-04-04 16:56:17 +0300

    preparing to deploy, bug fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Calculator from "@component/components/Calculator";
 export default function Home() {
   return (
-    <div className={"grid place-items-center h-screen"}>
+    <div className={"grid place-items-center h-screen font-gemunuLibre"}>
     <Calculator />
     </div>
   )
